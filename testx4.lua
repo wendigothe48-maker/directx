dc8bb9c1d1f07c2a26838a83af079fc2d51c406a
wendigothe48-maker <wendigothe48@gmail.com>
2026-05-24 21:44:08 +0530

    Add testx4.lua

---
+++ testx4.lua
@@ -0,0 +1,12 @@
+delay 1
+wendigokok@gmail.com
+ealmwlqvfwysisxy
+cosmicnitro54@gmail.com
+mtnngwceebhbkajm
+This is Simple Working Script
+$<title>
+100% Working 
+-- This Script Has Been Made By Vulcan Hub.
+-- This Script Has Not a Common key system so the Key link is not valid
+For Verification Purpose.
+-- Execute script and Click on Get link And More Info Shared On the link like how to get key.
